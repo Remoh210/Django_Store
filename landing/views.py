977b543af375571ed0ee3993de2ee251940189ef
PC <l2myfgame@mail.ru>
2017-05-31 23:10:51 +0900

    hello

---
+++ landing/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+
+def landing(request):
+    name = "Remoh210"
+    current_day = "05.01.2017"
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+
+        print(form.cleaned_data)
+        new_form = form.save()
+    return render(request, 'landing.html', locals())
